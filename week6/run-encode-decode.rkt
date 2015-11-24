afc3fca7a80bfc9033c67dcc294070709b2793e0
Yavor <jinninbin@gmail.com>
2015-11-24 03:54:07 +0200

    Add a better version for the encode function

--- week6/run-encode-decode.rkt
+++ week6/run-encode-decode.rkt
@@ -2,10 +2,10 @@
 
 (require "../week5/take-while.rkt")
 (require "../week5/drop-while.rkt")
+(require "group.rkt")
 
 ;Encode
-;It is possible to be done in a simpler manner (I guess) :)
-(define (run-length-encode str)
+(define (run-length-encode str) ;beta version(not using group)
   (letrec ([repeats-first (lambda (items)
                             (let ([len (length (take-while (lambda (x) (equal? x (first items))) items))])
                               (if (= len 1) "" len)))]
@@ -15,8 +15,18 @@
                                        (encode (drop-while (lambda (x) (equal? x (first items))) items)))]))])
     (apply ~a (apply append (encode (string->list str))))))
 
+;Decent Version of Encode
+(define (run-length-encode2 str) ;version 0.1(using group)
+  (define (iter l result)
+    (if (null? l) result
+        (let ([len (length (first l))]
+              [firstLetter (~a (first (first l)))])
+          (cond [(= len 1) (iter (rest l) (string-append result firstLetter))]
+                [else (iter (rest l) (string-append result (string-append (~a len) firstLetter)))]))))
+  (iter (group (string->list str)) ""))
+
 ;Decode
-(define (run-length-decode str)
+(define (run-length-decode str) ;beta version
   (letrec ([string-repeat (lambda (str times)
                             (cond [(= times 0) ""]
                                   [else (string-append str (string-repeat str (- times 1)))]))]
