57c6924c30710f157b1efa51f992f3a8e3467b36
Yavor <jinninbin@gmail.com>
2016-01-20 01:52:15 +0200

    Add working version of the gaussian elimination

--- Lights Out - Project/GaussianElimination.hs
+++ Lights Out - Project/GaussianElimination.hs
@@ -5,23 +5,44 @@ module GaussianElimination
 import TypesAndParams
 import Utils
 import Data.Bits(xor)
+import Data.List
+import ToggleMatrix
+
+test :: BoolMat
+test = addColumnBack (concat testBoard) toggleMat
 
 testBoard :: BoolMat
-testBoard =  [[True, True, False, False, True],
-			  [False, False, False, False, False],
-			  [False, True, True, True, False],
-			  [False, True, True, True, True],
-			  [True, False, True, False, False]]
+testBoard =  [[True, False, True, False, False],
+			  [True, False, True, False, True],
+			  [False, False, False, True, True],
+			  [False, False, True, True, True],
+			  [False, False, True, True, False]]
 
---Returns the index of the pivot row
-findPivotRow :: Int -> BoolMat -> Int
-findPivotRow currentCol = length.takeWhile (==False).(takeNthCol currentCol)
+--Returns the index of the next pivot row
+findPivot :: Int -> Int -> BoolMat -> Int
+findPivot startRow currCol = (\x -> x + startRow).length.takeWhile (==False).drop startRow.takeNthCol currCol
 
-addTwoRows :: Int -> Int -> BoolMat -> BoolMat
-addTwoRows pivotRow currentRow matrix = updateRow currentRow (zipWith xor (matrix !! pivotRow) (matrix !! currentRow)) matrix
+--Sums up two rows using xor
+sumTwoRows :: BoolList -> BoolList -> BoolList
+sumTwoRows pivotList ys
+	| not (ys !! (length (takeWhile (==False) pivotList))) = ys
+	| otherwise = zipWith xor pivotList ys
 
-gaussianElimination :: BoolMat -> BoolMat  
-gaussianElimination matrix 
+makeEchelon :: Int -> BoolMat -> BoolMat
+makeEchelon numCol matrix
+	| pivotIndex < length matrix && pivotIndex > -1 = upper ++ (pivot:(map (\x -> sumTwoRows pivot x) bottom))
+	| otherwise = matrix
+		where 
+			(upper, pivot : bottom) = splitAt numCol (swapRows numCol pivotIndex matrix)
+			pivotIndex = (findPivot numCol numCol matrix)
+
+gaussianElimination :: BoolMat -> BoolMat
+gaussianElimination matrix = gaussianElmination' 0 matrix
+	where gaussianElmination' numCol mat
+		| numCol == (maxCols - 1) = mat
+		| otherwise = gaussianElmination' (numCol + 1) (makeEchelon numCol mat)
+
+
 
 
 
